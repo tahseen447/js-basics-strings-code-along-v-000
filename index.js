5d765042b223118538e19f78b81d870e7af83f1e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-06 22:09:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const currentUser = 'Grace Hopper';
-
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
